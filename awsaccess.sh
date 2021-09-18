be8436ad17fda49d6d4f7de47f64a7d7790b5d9c
Yongkang He <he@yongkang.cloud>
2021-09-18 10:11:53 +0800

    Create awsaccess.sh

---
+++ awsaccess.sh
@@ -0,0 +1,3 @@
+echo "Configure AWS Access Key ID and Secret Access Key"
+export AWS_ACCESS_KEY_ID="YOUR_AWS_ACCESS_KEY_ID"         #Update with your access key
+export AWS_SECRET_ACCESS_KEY="YOUR_AWS_SECRET_ACCESS_KEY" #Update with your secret key
