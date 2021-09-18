aa142d93c52e60c12fe378e274ef989ebc7c0162
Yongkang He <he@yongkang.cloud>
2021-09-18 14:01:44 +0800

    Update

--- awsprep.sh
+++ awsprep.sh
@@ -1,18 +1,23 @@
 echo "Install eksctl"
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-sudo mv /tmp/eksctl /usr/local/bin
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C ~/eks-k10
 
 echo "Install kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod 755 kubectl
 
 echo "Install helm"
-export VERIFY_CHECKSUM=false
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+wget https://get.helm.sh/helm-v3.7.0-linux-amd64.tar.gz
+tar -zxvf helm-v3.7.0-linux-amd64.tar.gz
+mv linux-amd64/helm .
+rm helm-v3.7.0-linux-amd64.tar.gz
+rm -rf linux-amd64
+echo "export PATH=$PATH:~/eks-k10" >> ~/.bashrc
+echo "alias k=kubectl" >> ~/.bashrc
+. ~/.bashrc
 
 echo "Generate ssh public key"
 ssh-keygen -q -f ~/.ssh/id_rsa -N ""
 
 echo "Configure AWS Access Key ID and Secret Access Key"
-export AWS_ACCESS_KEY_ID="YOUR_AWS_ACCESS_KEY_ID"         #Update with your access key
-export AWS_SECRET_ACCESS_KEY="YOUR_AWS_SECRET_ACCESS_KEY" #Update with your secret key
+#export AWS_ACCESS_KEY_ID="YOUR_AWS_ACCESS_KEY_ID"         #Update with your access key
+#export AWS_SECRET_ACCESS_KEY="YOUR_AWS_SECRET_ACCESS_KEY" #Update with your secret key
--- setenv.sh
+++ setenv.sh
@@ -1,6 +1,6 @@
 #-------Set the environment variables"
 export MY_CLUSTER=eks4yong1         #Customize your cluster name
-export MY_INSTANCE_TYPE=t4g.large   #Customize your favorite machine type
+export MY_INSTANCE_TYPE=t3a.large   #Customize your favorite machine type
 export MY_REGION=us-east-2          #Customize your favorite region
 export MY_ZONE=us-east-2c           #Customize your favorite zone
 export MY_BUCKET=k10bucket4yong1    #Customize your favorite bucket
