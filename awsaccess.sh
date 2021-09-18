cbbe28614a0fea04681b837415806c387b4f65b5
Yongkang He <he@yongkang.cloud>
2021-09-18 10:33:31 +0800

    Update awsaccess.sh

--- awsaccess.sh
+++ awsaccess.sh
@@ -2,6 +2,9 @@ echo "Install eksctl"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 
+echo "Generate ssh public key"
+ssh-keygen -q -f ~/.ssh/id_rsa -N ""
+
 echo "Configure AWS Access Key ID and Secret Access Key"
 export AWS_ACCESS_KEY_ID="YOUR_AWS_ACCESS_KEY_ID"         #Update with your access key
 export AWS_SECRET_ACCESS_KEY="YOUR_AWS_SECRET_ACCESS_KEY" #Update with your secret key
