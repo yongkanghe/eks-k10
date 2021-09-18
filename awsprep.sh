2c5499499065134568c91005a5cf1c9d4d840ee3
Yongkang He <he@yongkang.cloud>
2021-09-18 14:30:23 +0800

    Update awsprep.sh

--- awsprep.sh
+++ awsprep.sh
@@ -19,5 +19,4 @@ echo "Generate ssh public key"
 ssh-keygen -q -f ~/.ssh/id_rsa -N ""
 
 echo "Configure AWS Access Key ID and Secret Access Key"
-#export AWS_ACCESS_KEY_ID="YOUR_AWS_ACCESS_KEY_ID"         #Update with your access key
-#export AWS_SECRET_ACCESS_KEY="YOUR_AWS_SECRET_ACCESS_KEY" #Update with your secret key
+
