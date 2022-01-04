52b07e23b04905f5643579b32323c3e8daba5c07
Yongkang He <he@yongkang.cloud>
2022-01-04 16:43:24 +0800

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,9 +1,9 @@
 #-------Set the environment variables"
 export MY_CLUSTER=eks4yong1         #Customize your cluster name
 export MY_INSTANCE_TYPE=t3a.large   #Customize your favorite machine type
-export MY_REGION=us-east-2          #Customize your favorite region
-export MY_ZONE=us-east-2c           #Customize your favorite zone
+export MY_REGION=ap-southeast-2          #Customize your favorite region
+export MY_ZONE=ap-southeast-2c           #Customize your favorite zone
 export MY_BUCKET=k10bucket4yong1    #Customize your favorite bucket
 export MY_OBJECT_STORAGE_PROFILE=myaws3 #Customize your favorite profile name
-export MY_K8S_VERSION=1.20              #Customize your Kubernetes Version
+export MY_K8S_VERSION=1.21              #Customize your Kubernetes Version
 
