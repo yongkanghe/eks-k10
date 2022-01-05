6d1357853c2bf0efe2b8f4565ac6249cd4b9055d
Yongkang He <he@yongkang.cloud>
2022-01-05 10:45:38 +0800

    Update k10-destroy.sh

--- k10-destroy.sh
+++ k10-destroy.sh
@@ -9,14 +9,14 @@ helm uninstall k10 -n kasten-io
 kubectl delete ns k10-cassandra
 kubectl delete ns kasten-io
 
-# echo '-------Deleting EBS Volumes'
-# aws ec2 describe-volumes --region $MY_REGION --query "Volumes[*].{ID:VolumeId}" --filters Name=tag:eks:cluster-name,Values=$(cat eks_clustername) | grep ID | awk '{print $2}' > ebs.list
-# aws ec2 describe-volumes --region $MY_REGION --query "Volumes[*].{ID:VolumeId}" --filters Name=tag:kubernetes.io/cluster/$(cat eks_clustername),Values=owned | grep ID | awk '{print $2}' >> ebs.list
-# for i in $(sed 's/\"//g' ebs.list);do echo $i;aws ec2 delete-volume --volume-id $i;done
+echo '-------Deleting EBS Volumes'
+aws ec2 describe-volumes --region $MY_REGION --query "Volumes[*].{ID:VolumeId}" --filters Name=tag:eks:cluster-name,Values=$(cat eks_clustername) | grep ID | awk '{print $2}' > ebs.list
+aws ec2 describe-volumes --region $MY_REGION --query "Volumes[*].{ID:VolumeId}" --filters Name=tag:kubernetes.io/cluster/$(cat eks_clustername),Values=owned | grep ID | awk '{print $2}' >> ebs.list
+for i in $(sed 's/\"//g' ebs.list);do echo $i;aws ec2 delete-volume --volume-id $i;done
 
-# echo '-------Deleting snapshots'
-# aws ec2 describe-snapshots --owner self --query "Snapshots[*].{ID:SnapshotId}" --filters Name=tag:kanister.io/clustername,Values=$clusterid | grep ID | awk '{print $2}' > ebssnap.list
-# for i in $(sed 's/\"//g' ebssnap.list);do echo $i;aws ec2 delete-snapshot --snapshot-id $i;done
+echo '-------Deleting snapshots'
+aws ec2 describe-snapshots --owner self --query "Snapshots[*].{ID:SnapshotId}" --filters Name=tag:kanister.io/clustername,Values=$clusterid | grep ID | awk '{print $2}' > ebssnap.list
+for i in $(sed 's/\"//g' ebssnap.list);do echo $i;aws ec2 delete-snapshot --snapshot-id $i;done
 
 echo '-------Deleting objects from the bucket'
 aws s3 rb s3://$(cat eks_bucketname) --force
