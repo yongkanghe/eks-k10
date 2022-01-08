b309565e5d85c87a8c8c5fa4339cb4ac87bd23d5
Yongkang He <he@yongkang.cloud>
2022-01-08 20:29:24 +0800

    Update k10status.sh

--- k10status.sh
+++ k10status.sh
@@ -1,6 +1,7 @@
 #This is the script for you to verify the backup and export status of the k10- namespace
 #Run ./k10status, shortly, the backup and and export status will be reported.
 
+my_namespace=$(kubectl get ns | grep k10- | awk '{print $1}')
 kubectl get backupactions.actions.kio.kasten.io -n $my_namespace | grep -v NAME | head -1 | awk '{print $1}' > backupname
 echo "The backup job status is $(kubectl get backupactions.actions.kio.kasten.io -n $my_namespace $(cat backupname) -ojsonpath="{.status.state}{'\n'}")"
 kubectl get exportactions.actions.kio.kasten.io -n $my_namespace | grep -v NAME | head -1 | awk '{print $1}' > exportname
