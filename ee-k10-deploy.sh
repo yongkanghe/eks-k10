a847d65caa6a7b287145db063bd3b9ce604af7da
Yongkang He <he@yongkang.cloud>
2022-01-08 20:06:46 +0800

    Update ee-k10-deploy.sh

--- ee-k10-deploy.sh
+++ ee-k10-deploy.sh
@@ -74,7 +74,9 @@ cat eks-token
 
 endtime=$(date +%s)
 duration=$(( $endtime - $starttime ))
-echo "-------Total time is $(($duration / 60)) minutes $(($duration % 60)) seconds."
+echo "-------Total time for K10+DB+Policy is $(($duration / 60)) minutes $(($duration % 60)) seconds."
 echo "" | awk '{print $1}'
 echo "-------Created by Yongkang"
 echo "-------Email me if any suggestions or issues he@yongkang.cloud"
+echo "" | awk '{print $1}'
+echo "" | awk '{print $1}'
