c280809dd8147463ca705054950b89d8eb274e8e
Yongkang He <he@yongkang.cloud>
2022-01-10 14:21:17 +0800

    Update awsprep.sh

--- awsprep.sh
+++ awsprep.sh
@@ -20,15 +20,20 @@ if [ ! -f ~/.ssh/id_rsa ]; then
   ssh-keygen -q -f ~/.ssh/id_rsa -N ""
 fi
 
+aws sts get-caller-identity | grep assumed-role
+if [ `echo $?` -eq 1 ]
+then
+  echo -n "Enter your AWS Access Key ID and press [ENTER]: "
+  read AWS_ACCESS_KEY_ID
+  echo "" | awk '{print $1}'
+  echo $AWS_ACCESS_KEY_ID > awsaccess
+  echo -n "Enter your AWS Secret Access Key and press [ENTER]: "
+  read AWS_SECRET_ACCESS_KEY
+  echo $AWS_SECRET_ACCESS_KEY >> awsaccess
+fi
+
 clear
 
-echo -n "Enter your AWS Access Key ID and press [ENTER]: "
-read AWS_ACCESS_KEY_ID
-echo "" | awk '{print $1}'
-echo $AWS_ACCESS_KEY_ID > awsaccess
-echo -n "Enter your AWS Secret Access Key and press [ENTER]: "
-read AWS_SECRET_ACCESS_KEY
-echo $AWS_SECRET_ACCESS_KEY >> awsaccess
 echo "" | awk '{print $1}'
 echo "You are ready to deploy now!"
 echo "" | awk '{print $1}'
