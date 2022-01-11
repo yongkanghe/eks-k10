b6ba273f38a3178d90f96af85e45735da17f08bc
Yongkang He <he@yongkang.cloud>
2022-01-11 09:51:13 +0800

    Update migration-s3-location.sh

--- migration-s3-location.sh
+++ migration-s3-location.sh
@@ -2,16 +2,16 @@
 
 export MY_REGION=ap-southeast-2
 export MY_OBJECT_STORAGE_PROFILE=myaws3-migration
-export AWS_ACCESS_KEY_ID=$(cat awsaccess | head -1)
-export AWS_SECRET_ACCESS_KEY=$(cat awsaccess | tail -1)
+# export AWS_ACCESS_KEY_ID=$(cat awsaccess | head -1)
+# export AWS_SECRET_ACCESS_KEY=$(cat awsaccess | tail -1)
 echo k10migration4yong1 > k10_migration_bucketname
 
 echo '-------Creating a S3 profile secret'
 kubectl create secret generic k10-s3-secret \
       --namespace kasten-io \
       --type secrets.kanister.io/aws \
-      --from-literal=aws_access_key_id=$AWS_ACCESS_KEY_ID \
-      --from-literal=aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
+      --from-literal=aws_access_key_id=$(cat awsaccess | head -1) \
+      --from-literal=aws_secret_access_key=$(cat awsaccess | tail -1)
 
 echo '-------Creating a S3 profile'
 cat <<EOF | kubectl apply -f -
