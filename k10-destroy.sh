35758501d5455dc7e6a64f3c6aad9f9225980b02
Yongkang He <he@yongkang.cloud>
2022-01-14 15:41:40 +0800

    Update k10-destroy.sh

--- k10-destroy.sh
+++ k10-destroy.sh
@@ -1,6 +1,6 @@
 starttime=$(date +%s)
 . ./setenv.sh
-echo '-------Deleting Cassandra and Kasten K10'
+
 clusterid=$(kubectl get namespace default -ojsonpath="{.metadata.uid}{'\n'}")
 # eksctl delete cluster --name $(cat k10_eks_clustername) --region $MY_REGION
 
@@ -8,9 +8,12 @@ aws iam get-role --role-name k10-iam-role4yong1 | grep role4yong1
 
 if [ `echo $?` -eq 0 ]
 then
+  echo '-------Deleting AWS IAM role and policy if any'
   aws iam delete-role-policy --role-name k10-iam-role4yong1 --policy-name k10-iam-policy4yong1
   aws iam delete-role --role-name k10-iam-role4yong1 
 fi
+
+echo '-------Deleting Cassandra and Kasten K10'
 
 helm uninstall cassandra -n k10-cassandra
 helm uninstall k10 -n kasten-io
