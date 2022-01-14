710ee3deea80e72f9ce75daef6803c5b87526ffc
Yongkang He <he@yongkang.cloud>
2022-01-14 09:50:15 +0800

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -6,4 +6,3 @@ export MY_ZONE=ap-southeast-2c           #Customize your favorite zone
 export MY_BUCKET=k10bucket4yong1    #Customize your favorite bucket
 export MY_OBJECT_STORAGE_PROFILE=myaws3 #Customize your favorite profile name
 export MY_K8S_VERSION=1.21              #Customize your Kubernetes Version
-
