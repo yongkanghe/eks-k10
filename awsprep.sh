3551f619cb67d70802c956add1c00c52e496199c
Yongkang He <he@yongkang.cloud>
2022-01-14 09:50:12 +0800

    Update awsprep.sh

--- awsprep.sh
+++ awsprep.sh
@@ -1,19 +1,25 @@
-echo "Install eksctl"
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C ~/eks-k10
+if [ ! -f ~/eks-k10/eksctl ]; then
+  echo "Install eksctl"
+  curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C ~/eks-k10
+fi
 
-echo "Install kubectl"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod 755 kubectl
+if [ ! -f ~/eks-k10/kubectl ]; then
+  echo "Install kubectl"
+  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  chmod 755 kubectl
+fi
 
-echo "Install helm"
-wget https://get.helm.sh/helm-v3.7.0-linux-amd64.tar.gz
-tar -zxvf helm-v3.7.0-linux-amd64.tar.gz
-mv linux-amd64/helm .
-rm helm-v3.7.0-linux-amd64.tar.gz
-rm -rf linux-amd64
-echo "export PATH=$PATH:~/eks-k10" >> ~/.bashrc
-echo "alias k=kubectl" >> ~/.bashrc
-. ~/.bashrc
+if [ ! -f ~/eks-k10/helm ]; then
+  echo "Install helm"
+  wget https://get.helm.sh/helm-v3.7.0-linux-amd64.tar.gz
+  tar -zxvf helm-v3.7.0-linux-amd64.tar.gz
+  mv linux-amd64/helm .
+  rm helm-v3.7.0-linux-amd64.tar.gz
+  rm -rf linux-amd64
+  echo "export PATH=$PATH:~/eks-k10" >> ~/.bashrc
+  echo "alias k=kubectl" >> ~/.bashrc
+  . ~/.bashrc
+fi
 
 echo "Generate ssh public key if not existing"
 if [ ! -f ~/.ssh/id_rsa ]; then
