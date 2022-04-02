81fd39c286df36ca27a749d9f04f8ab9736bf24c
Yongkang He <he@yongkang.cloud>
2022-04-02 18:39:08 +0800

    Update ee-deploy.sh

--- ee-deploy.sh
+++ ee-deploy.sh
@@ -11,6 +11,3 @@ endtime=$(date +%s)
 duration=$(( $endtime - $starttime ))
 echo "-------Total time for EKS+k10 deployment is $(($duration / 60)) minutes $(($duration % 60)) seconds."
 echo "" | awk '{print $1}'
-echo "-------Created by Yongkang"
-echo "-------Email me if any suggestions or issues he@yongkang.cloud"
-echo "" | awk '{print $1}'
