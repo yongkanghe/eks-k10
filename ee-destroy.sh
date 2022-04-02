fc84c63e63947d05196e38ee718c00db7a6fc96b
Yongkang He <he@yongkang.cloud>
2022-04-02 18:02:03 +0800

    Update ee-destroy.sh

--- ee-destroy.sh
+++ ee-destroy.sh
@@ -7,16 +7,16 @@ aws iam delete-role --role-name k10-iam-role4yong1
 
 echo '-------Deleting the EKS Cluster (typically in ~ 10 mins)'
 clusterid=$(kubectl get namespace default -ojsonpath="{.metadata.uid}{'\n'}")
-eksctl delete cluster --name $(cat k10_eks_clustername) --region $MY_REGION
+eksctl delete cluster --name $(cat k10_eks_clustername) --region $AWS_REGION
 
 echo '-------Deleting EBS Volumes'
-aws ec2 describe-volumes --region $MY_REGION --query "Volumes[*].{ID:VolumeId}" --filters Name=tag:eks:cluster-name,Values=$(cat k10_eks_clustername) | grep ID | awk '{print $2}' > ebs.list
-aws ec2 describe-volumes --region $MY_REGION --query "Volumes[*].{ID:VolumeId}" --filters Name=tag:kubernetes.io/cluster/$(cat k10_eks_clustername),Values=owned | grep ID | awk '{print $2}' >> ebs.list
-for i in $(sed 's/\"//g' ebs.list);do echo $i;aws ec2 delete-volume --volume-id $i --region $MY_REGION;done
+aws ec2 describe-volumes --region $AWS_REGION --query "Volumes[*].{ID:VolumeId}" --filters Name=tag:eks:cluster-name,Values=$(cat k10_eks_clustername) | grep ID | awk '{print $2}' > ebs.list
+aws ec2 describe-volumes --region $AWS_REGION --query "Volumes[*].{ID:VolumeId}" --filters Name=tag:kubernetes.io/cluster/$(cat k10_eks_clustername),Values=owned | grep ID | awk '{print $2}' >> ebs.list
+for i in $(sed 's/\"//g' ebs.list);do echo $i;aws ec2 delete-volume --volume-id $i --region $AWS_REGION;done
 
 echo '-------Deleting snapshots'
-aws ec2 describe-snapshots --owner self --query "Snapshots[*].{ID:SnapshotId}" --filters Name=tag:kanister.io/clustername,Values=$clusterid --region $MY_REGION | grep ID | awk '{print $2}' > ebssnap.list
-for i in $(sed 's/\"//g' ebssnap.list);do echo $i;aws ec2 delete-snapshot --snapshot-id $i --region $MY_REGION;done
+aws ec2 describe-snapshots --owner self --query "Snapshots[*].{ID:SnapshotId}" --filters Name=tag:kanister.io/clustername,Values=$clusterid --region $AWS_REGION | grep ID | awk '{print $2}' > ebssnap.list
+for i in $(sed 's/\"//g' ebssnap.list);do echo $i;aws ec2 delete-snapshot --snapshot-id $i --region $AWS_REGION;done
 
 echo '-------Deleting objects from the bucket'
 aws s3 rb s3://$(cat k10_eks_bucketname) --force
