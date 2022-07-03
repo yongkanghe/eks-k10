ef87537f3b35a3ee156e95b449b6830d8601e3f4
Yongkang He <he@yongkang.cloud>
2022-07-03 12:50:50 +0800

    Update migration-s3-location.sh

--- migration-s3-location.sh
+++ migration-s3-location.sh
@@ -4,7 +4,8 @@
 export MY_OBJECT_STORAGE_PROFILE=myaws3-migration
 # export AWS_ACCESS_KEY_ID=$(cat awsaccess | head -1)
 # export AWS_SECRET_ACCESS_KEY=$(cat awsaccess | tail -1)
-echo k10migration4yong1-$(cat awsaccess | (read -n 2 i; echo $i;))$(date +%m%d) | awk '{print tolower($0)}' > k10_migration_bucketname
+LAST4=$(echo -n $(cat awsaccess) | tail -c4)
+echo k10migration4yong1-$LAST4$(date +%m%d) | awk '{print tolower($0)}' > k10_migration_bucketname
 
 echo '-------Creating a S3 profile secret'
 kubectl create secret generic k10-s3-secret \
