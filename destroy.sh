57461e1ee3d5fabf714a3dee9a4efd5ca6c38bf9
Yongkang He <he@yongkang.cloud>
2022-07-03 13:04:31 +0800

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -15,6 +15,7 @@ for i in $(sed 's/\"//g' ebssnap.list);do echo $i;aws ec2 delete-snapshot --snap
 
 echo '-------Deleting objects from the bucket'
 aws s3 rb s3://$(cat k10_eks_bucketname) --force
+aws s3 rb s3://$(cat k10_migration_bucketname) --force
 
 echo '-------Deleting kubeconfig for this cluster'
 kubectl config delete-context $(kubectl config get-contexts | grep $(cat k10_eks_clustername) | awk '{print $2}')
