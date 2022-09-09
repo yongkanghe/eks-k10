dc6e340d5820e13cf4d86439bce81e0efb08845a
Yongkang He <he@yongkang.cloud>
2022-09-09 16:37:55 +0800

    Create eks2ocp-cassandra-policy.sh

---
+++ eks2ocp-cassandra-policy.sh
@@ -0,0 +1,66 @@
+export MY_OBJECT_STORAGE_PROFILE=myaws3-migration
+
+echo '------Create backup policies'
+cat <<EOF | kubectl apply -f -
+apiVersion: config.kio.kasten.io/v1alpha1
+kind: Policy
+metadata:
+  name: eks2ocp-cassandra-backup
+  namespace: kasten-io
+spec:
+  comment: ""
+  frequency: "@hourly"
+  actions:
+    - action: backup
+      backupParameters:
+        profile:
+          namespace: kasten-io
+          name: $MY_OBJECT_STORAGE_PROFILE
+    - action: export
+      exportParameters:
+        frequency: "@hourly"
+        migrationToken:
+          name: ""
+          namespace: ""
+        profile:
+          name: $MY_OBJECT_STORAGE_PROFILE
+          namespace: kasten-io
+        receiveString: ""
+        exportData:
+          enabled: true
+      retention:
+        hourly: 0
+        daily: 0
+        weekly: 0
+        monthly: 0
+        yearly: 0
+  retention:
+    hourly: 4
+    daily: 1
+    weekly: 1
+    monthly: 0
+    yearly: 0
+  selector:
+    matchExpressions:
+      - key: k10.kasten.io/appNamespace
+        operator: In
+        values:
+          - k10-cassandra
+EOF
+
+sleep 5
+
+echo '-------Kickoff the on-demand backup job'
+sleep 5
+
+cat <<EOF | kubectl create -f -
+apiVersion: actions.kio.kasten.io/v1alpha1
+kind: RunAction
+metadata:
+  generateName: run-backup-
+spec:
+  subject:
+    kind: Policy
+    name: eks2ocp-cassandra-backup
+    namespace: kasten-io
+EOF
